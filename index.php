2f7db0b19be703d6eb796f7e9ac8fdd600d9261b
PawitSahare <123424956+pawitpr@users.noreply.github.com>
2023-05-17 07:13:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,45 @@
+<?php
+// File: index.php
+
+// Directory to serve files from
+$dir = './cdn';
+
+// Get the file name from the query parameter
+if (isset($_GET['file'])) {
+  $filename = basename($_GET['file']);
+  $filepath = $dir . '/' . $filename;
+
+  // Check if the file exists and is a file (not a directory)
+  if (file_exists($filepath) && is_file($filepath)) {
+    // Set appropriate headers for file download
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($filepath));
+    readfile($filepath);
+    exit;
+  }
+}
+
+// List files in the directory
+$files = scandir($dir);
+?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>CDN File Server</title>
+  <title>HTTP File Server</title>
 </head>
 <body>
-  <h1>CDN File Server</h1>
+  <h1>HTTP File Server</h1>
   <ul>
-    <!-- Iterate over files in the ./cdn directory -->
-    <!-- Modify the base path as per your project structure -->
-    <?php
-      $files = glob('./cdn/*');
-      foreach ($files as $file) {
-        $filename = basename($file);
-        echo "<li><a href='./cdn/$filename' download>$filename</a></li>";
-      }
-    ?>
+    <?php foreach ($files as $file): ?>
+      <?php if ($file !== '.' && $file !== '..'): ?>
+        <li><a href="?file=<?= urlencode($file) ?>"><?= $file ?></a></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
   </ul>
 </body>
 </html>
